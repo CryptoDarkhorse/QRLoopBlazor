e0c3fef4618303d34141a4089a02dd8e5fd9b313
Cameron <c.knight8817@gmail.com>
2022-06-30 22:45:48 +0300

    Change QRGenerator to use BlazorZXingJs, test QRReader with mock data

--- QRLoopDemo/js/src/index.js
+++ QRLoopDemo/js/src/index.js
@@ -6,12 +6,24 @@
     getProgress
 } from './my_lib';
 
+import { Buffer } from "buffer";
+
 export function GetFrames(data, dataSize = 120, loops = 1) {
     return getFrames(data, dataSize, loops);
 }
 
 export function ParseFrames(s, chunk) {
-    return parseFrames(s, chunk);
+    s = parseFrames(s, chunk);
+    for (var j = 0; j < s.frames.length; j++) {
+        if (!Buffer.isBuffer(s.frames[j].data)) {
+            s.frames[j].data = Buffer.from(s.frames[j].data);
+        }
+    }
+    if (isComplete(s)) {
+        return ["1", getFrameData(s).toString()];
+    } else {
+        return ["0", s];
+    }
 }
 
 export function IsComplete(s) {
@@ -19,7 +31,7 @@ export function IsComplete(s) {
 }
 
 export function GetFrameData(s) {
-    return getFrameData(s);
+    return getFrameData(s).toString();
 }
 
 export function GetProgress(s) {
