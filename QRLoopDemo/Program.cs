578f7d4a3d60298fb85c5147df70c034b86d1048
Cameron <c.knight8817@gmail.com>
2022-06-30 11:46:37 +0300

    QR code generation using Syncfusion

--- QRLoopDemo/Program.cs
+++ QRLoopDemo/Program.cs
@@ -2,10 +2,16 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using QRLoopDemo;
 
+// Syncfusion QR code gen
+using Syncfusion.Blazor;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Syncfusion
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
 
 await builder.Build().RunAsync();
