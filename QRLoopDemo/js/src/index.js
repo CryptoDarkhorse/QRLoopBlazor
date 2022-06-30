2457efa757b4f9f406a2e2e517645e882bf99bba
Cameron <c.knight8817@gmail.com>
2022-06-30 19:08:11 +0300

    QRReader using BlazorZXingJs

--- QRLoopDemo/js/src/index.js
+++ QRLoopDemo/js/src/index.js
@@ -1,5 +1,27 @@
-﻿import { getFrames } from './my_lib';
+﻿import {
+    getFrames,
+    parseFrames,
+    isComplete,
+    getFrameData,
+    getProgress
+} from './my_lib';
 
 export function GetFrames(data, dataSize = 120, loops = 1) {
     return getFrames(data, dataSize, loops);
+}
+
+export function ParseFrames(s, chunk) {
+    return parseFrames(s, chunk);
+}
+
+export function IsComplete(s) {
+    return isComplete(s);
+}
+
+export function GetFrameData(s) {
+    return getFrameData(s);
+}
+
+export function GetProgress(s) {
+    return getProgress(s);
 }
--- QRLoopDemo/js/src/my_lib.js
+++ QRLoopDemo/js/src/my_lib.js
@@ -1,5 +1,27 @@
-﻿import { dataToFrames } from 'qrloop';
+﻿import {
+    dataToFrames,
+    parseFramesReducer,
+    areFramesComplete,
+    framesToData,
+    progressOfFrames
+} from "qrloop";
 
 export function getFrames(data, dataSize, loops) {
     return dataToFrames(data, dataSize, loops);
+}
+
+export function parseFrames(s, chunk) {
+    return parseFramesReducer(s, chunk);
+}
+
+export function isComplete(s) {
+    return areFramesComplete(s);
+}
+
+export function getFrameData(s) {
+    return framesToData(s);
+}
+
+export function getProgress(s) {
+    return progressOfFrames(s);
 }
